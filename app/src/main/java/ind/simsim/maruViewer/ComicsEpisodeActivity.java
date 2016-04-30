bb09c4e779aad33bbec9a70a25cecd25eac79a95
sesna99 <sesna99@gmail.com>
2016-04-30 22:16:34 +0900

    episode

--- app/src/main/java/ind/simsim/maruViewer/ComicsEpisodeActivity.java
+++ app/src/main/java/ind/simsim/maruViewer/ComicsEpisodeActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -11,15 +12,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
@@ -27,6 +27,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -36,7 +40,6 @@ public class ComicsEpisodeActivity extends Activity {
     private ListView listView;
     private View header;
     private ImageView image;
-    private ComicsEpisodeAdapter adapter;
     private ArrayList<String> episode;
     private ArrayList<String> link;
     private Episode task;
@@ -44,7 +47,13 @@ public class ComicsEpisodeActivity extends Activity {
     private int dWidth, dHeight;
     private Intent intent;
     private Context mContext;
-    private ComicsEpisodeAdapter.ViewHolder holder;
+    private WebView webView;
+    private WebSettings settings;
+    private File file;
+    private FileWriter fw;
+    private BufferedWriter bw;
+    private String path;
+    private StringBuilder html;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +71,7 @@ public class ComicsEpisodeActivity extends Activity {
         View mCustomView = LayoutInflater.from(this).inflate(R.layout.custom_actionbar, null);
         actionBar.setCustomView(mCustomView);
 
-        ImageButton imageButton = (ImageButton)mCustomView.findViewById(R.id.imageView);
+        ImageButton imageButton = (ImageButton) mCustomView.findViewById(R.id.imageView);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,23 +79,36 @@ public class ComicsEpisodeActivity extends Activity {
             }
         });
 
-
-        listView = (ListView) findViewById(R.id.listView);
-        header = getLayoutInflater().inflate(R.layout.comics_episode_header, null, false);
-        image = (ImageView)header.findViewById(R.id.imageView);
         url = getIntent().getStringExtra("url");
         url = url.contains("marumaru") ? url : "http://marumaru.in" + url;
-        episode = new ArrayList<>();
-        link = new ArrayList<>();
+        /*listView = (ListView) findViewById(R.id.listView);
+        header = getLayoutInflater().inflate(R.layout.comics_episode_header, null, false);
+        image = (ImageView)header.findViewById(R.id.imageView);
         adapter = new ComicsEpisodeAdapter(this, R.layout.comics_episode_item, episode);
         listView.addHeaderView(header);
-        listView.setAdapter(adapter);
+        listView.setAdapter(adapter);*/
+        episode = new ArrayList<>();
+        link = new ArrayList<>();
+
+        path = getCacheDir() + "/episode.html";
+        file = new File(path);
+        try {
+            fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
         dWidth = dm.widthPixels;
         dHeight = dm.heightPixels / 2;
 
         mContext = this;
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new CustomWebViewClient());
+        settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
 
         task = new Episode();
         task.execute();
@@ -104,35 +126,44 @@ public class ComicsEpisodeActivity extends Activity {
             try {
                 long startTime = System.currentTimeMillis();
                 Document document = Jsoup.connect(url).timeout(0).get();
-                Elements elements = document.select("div a[href*=shencomics]");
-                Elements elements2 = document.select("div img[src*=quickimage]");
-                Elements elements3 = document.select("a");
+                Elements episode = document.select("div[style*=text-align]");
+                Elements script = document.select("script");
+                Elements image = document.select("div img[src*=quickimage]");
+                html = new StringBuilder();
+                html.append("<html><body><div style=\"text-align: center\"><img src=\"").append(image.attr("src")).append("\" width=").append(dWidth / 3.5).append(" height=").append(dHeight / 2.5).append("></div><br>");
+                html.append("<div style=\"text-align: center;\" align=\"center\">");
+                html.append(episode.html());
+                html.append("</div>");
 
-                int size = elements.size();
+                /*
+                Elements episode = document.select("div[align*=center] span");
+                Elements allEpisode = document.select("div[align*=center] a[href*=maru]");
+                Elements script = document.select("script");
+                Elements image = document.select("div img[src*=quickimage]");
+                html = new StringBuilder();
+                html.append("<html><body><div style=\"text-align: center\"><img src=\"").append(image.attr("src")).append(" width=").append(dWidth / 3.5).append(" height=").append(dHeight / 2.5).append("></div><br>");
+
+                int size = episode.size();
                 Log.i("elementsSize", size + "");
-                for(int i = 0; i < size; i++) {
-                    if(elements.get(i).text().equals("")){
+                for (int i = 0; i < size; i++) {
+                    if (episode.get(i).attr("cf-token").equals(""))
                         continue;
-                    }
-                    episode.add(elements.get(i).text());
-                    link.add(elements.get(i).attr("href"));
+                    html.append("<span style=\"font-size: 14pt;\"><span target=\"_self\" cf-token=\"").append(episode.get(i).attr("cf-token")).append("\"><font color=\"#717171\">").append(episode.get(i).text()).append("</font></span></span><br>");
                 }
-                imageUrl = elements2.attr("src");
-                size = elements3.size();
-                for(int i = 0; i < size; i++) {
-                    if(!elements3.get(i).text().contains("전편")){
-                        continue;
-                    }
-                    episode.add(elements3.get(i).text());
-                    link.add(elements3.get(i).attr("href"));
-                    break;
-                }
+                //http://marumaru.in/?c=1/30&amp;cat=%EC%9B%94%EA%B0%84&amp;mod=view&amp;uid=41705" 전편 보러가기
+                if(!allEpisode.text().equals(""))
+                    html.append("<div align=\"center\" style=\"color: rgb(0, 0, 0); font-family: dotum; font-size: 12px; line-height: 19.2px;\">").append(" <a target=\"_blank\" href=\"").append(allEpisode.attr("href")).append("\" style=\"color: rgb(113, 113, 113); text-decoration: none; outline: 0px;\">").append(allEpisode.text()).append("</a></div></div> </div><br>");
+                html.append(script.get(16)).append("</body></html>");
+                 */
+
+                html.append(script.get(16)).append("</body></html>");
+                Log.i("html", html.toString());
 
                 long endTime = System.currentTimeMillis();
                 Log.i("time", (endTime - startTime) / 1000.f + "초");
 
                 document = null;
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
@@ -141,18 +172,21 @@ public class ComicsEpisodeActivity extends Activity {
 
         @Override
         protected void onPostExecute(Void mVoid) {
-            Glide.with(ComicsEpisodeActivity.this).load(imageUrl).into(image);
-            adapter.addEpisodeArray(episode);
-            adapter.refresh();
+            try {
+                bw.write(html.toString());
+                bw.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            webView.loadUrl("file://" + path);
         }
     }
 
-    class Comics extends AsyncTask<ArrayList<Object>, Void, Void> {
-        private Elements image;
+    class Comics extends AsyncTask<String, Void, Void> {
+        private Elements image, title;
         private StringBuilder html;
-        private int size, position;
-        private ComicsEpisodeAdapter.ViewHolder holder;
-        private ArrayList<Object> objects;
+        private String url;
+        private int size;
 
         @Override
         protected void onPreExecute() {
@@ -160,16 +194,14 @@ public class ComicsEpisodeActivity extends Activity {
         }
 
         @Override
-        protected Void doInBackground(ArrayList<Object>... params) {
-            objects = params[0];
-            holder = (ComicsEpisodeAdapter.ViewHolder) objects.get(0);
-            position = (int) objects.get(1);
-            Log.i("link", link.get(position)+"");
+        protected Void doInBackground(String... params) {
+            url = params[0];
             try {
                 long startTime = System.currentTimeMillis();
 
-                Document document = Jsoup.connect(link.get(position)).cookie("wp-postpass_e1ac6d6cb3b647764881f16d009c885c", "%24P%24B7TTtyw0aLlsT1XDbHUOnmABsLoItB0").timeout(0).userAgent("Mozilla/5.0").post();
+                Document document = Jsoup.connect(url).cookie("wp-postpass_e1ac6d6cb3b647764881f16d009c885c", "%24P%24B7TTtyw0aLlsT1XDbHUOnmABsLoItB0").timeout(0).userAgent("Mozilla/5.0").post();
                 image = document.select("img");
+                title = document.select("h1[class*=entry-title]");
 
                 html = new StringBuilder();
                 html.append(getResources().getString(R.string.htmlStart));
@@ -213,101 +245,40 @@ public class ComicsEpisodeActivity extends Activity {
         @Override
         protected void onPostExecute(Void mVoid) {
             intent = new Intent(mContext, ComicsViewer.class);
-            intent.putExtra("title", episode.get(position));
+            intent.putExtra("title", title.text());
             intent.putExtra("html", html.toString());
             startActivity(intent);
-            holder.episode.setText(episode.get(position));
         }
     }
 
-    class ComicsEpisodeAdapter extends BaseAdapter {
-        Context mContext;
-        LayoutInflater inflater;
-        int layout;
-        ArrayList<String> episode;
-
-        public ComicsEpisodeAdapter(Context context, int layout, ArrayList<String> episode) {
-            this.mContext = context;
-            inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            this.layout = layout;
-            this.episode = episode;
-        }
-
+    class CustomWebViewClient extends WebViewClient {
         @Override
-        public int getCount() {
-            return episode.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return episode.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        public void addEpisodeArray(ArrayList<String> episode){
-            this.episode = episode;
-        }
-
-        public void refresh(){
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            final ViewHolder holder;
-            final ArrayList<Object> objects = new ArrayList<>();;
-            if(convertView == null){
-                convertView = inflater.inflate(layout, parent, false);
-                holder = new ViewHolder();
-                holder.episode = (Button)convertView.findViewById(R.id.button);
-                convertView.setTag(holder);
-            }
-            else{
-                holder = (ViewHolder)convertView.getTag();
-            }
-
-            objects.add((Object) holder);
-            objects.add((Object) position);
-            holder.episode.setText(episode.get(position));
-            holder.episode.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = null;
-                    if(position == getCount() - 1){
-                        if(link.get(position).contains("marumaru")) {
-                            intent = new Intent(mContext, ComicsEpisodeActivity.class);
-                            intent.putExtra("url", link.get(position));
-                            startActivity(intent);
-                            finish();
-                        }
-                        else{
-                            holder.episode.setText("Loading");
-                            new Comics().execute(objects);
-                        }
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            try {
+                if(!view.getOriginalUrl().equals(url)){
+                    if (!url.contains("maru")) {
+                        view.stopLoading();
+                        view.goBack();
+                        new Comics().execute(url);
                     }
                     else{
-                        holder.episode.setText("Loading");
-                        new Comics().execute(objects);
+                        intent = new Intent(getApplicationContext(), ComicsEpisodeActivity.class);
+                        intent.putExtra("url", url);
+                        startActivity(intent);
+                        finish();
                     }
                 }
-            });
-
-            return convertView;
-        }
-
-        class ViewHolder{
-            private Button episode;
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         int itemId = item.getItemId();
-        Toast.makeText(ComicsEpisodeActivity.this, ""+itemId, Toast.LENGTH_SHORT).show();
+        Toast.makeText(ComicsEpisodeActivity.this, "" + itemId, Toast.LENGTH_SHORT).show();
         return true;
     }
 
